71cc1a536656fd5732cee242c4476cdb3e75412a
ecp-alex <alex.mackenzie@englishcollege.cz>
2019-11-05 06:43:14 +0100

    v2 | rewrite | class to functional (react > v16.8)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       {name: 'Alex', age: 40},
       {name: 'Kamil', age: 48},
       {name: 'Bruce', age: '??'}
-    ],
-    otherState: 'some other value'
-  }
+    ]
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState({otherState: 'some other value?'});
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
     // console.log('was clicked');
     // DON'T DO THIS: this.state.persons[0].name = 'Alex the GREAT';
-    this.setState({
+    setPersonsState({
       persons: [
         {name: 'Alex', age: 21},
         {name: 'Kamil', age: 21},
         {name: 'Bruce', age: 21}
-      ]
-    })
-  }
+      ],
+      otherState: personsState.otherState
+    });
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Make Alex Great Again</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: running</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
-    );
-    // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Working now?'))
-  }
+  return (
+    <div className="App">
+      <h1>Make Alex Great Again</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My hobbies: running</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+    </div>
+  );
+  // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Working now?'))
 }
 
-export default App;
+export default app;
+
