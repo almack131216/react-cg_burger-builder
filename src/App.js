ecf319dfb646f3d61ff9185921fdb84608a2cba3
ecp-alex <alex.mackenzie@englishcollege.cz>
2019-11-12 13:08:31 +0100

    73 | STYLE | removed styled-components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import "./App.css";
 import Person from "./Person/Person";
-
-const StyledButton = styled.button`
-  background-color: ${props => (props.alt ? "red" : "green")};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  &:hover {
-    background-color: ${props => (props.alt ? "salmon" : "lightgreen")};
-    color: black;
-  }
-`;
 
 class App extends Component {
   state = {
@@ -98,12 +84,9 @@ class App extends Component {
       <div className="App">
         <h1>Make Alex Great Again</h1>
         <p className={classes.join(" ")}>This is working!</p>
-        <StyledButton
-          alt={this.state.showPersons}
-          onClick={this.togglePersonsHandler}
-        >
+        <button className="button" onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </StyledButton>
+        </button>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,32 +1,15 @@
 import React from "react";
 // import "./Person.css";
-import styled from "styled-components";
-
-const StyledDiv = styled.div`
-  background-color: white;
-  width: 60%;
-  margin: 15px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 15px;
-  text-align: center;
-
-  @media (max-width: 500px) {
-    background-color: #f5f5f5;
-    border-color: black;
-  }
-`;
 
 const person = props => {
   return (
-    // <div className="Person" style={style}>
-    <StyledDiv>
+    <div className="Person">
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
+    </div>
   );
 };
 
